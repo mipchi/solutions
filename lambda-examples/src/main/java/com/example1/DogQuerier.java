cc3eef11f1b5b152bc2c6b29c04633446c5c1409
mipchi <31217056+mipchi@users.noreply.github.com>
2019-06-10 21:55:57 -0700

    removed all files

--- lambda-examples/src/main/java/com/example1/DogQuerier.java
+++
@@ -1,7 +0,0 @@
-package com.example1;
-
-//Our functional interface to test dogs
-@FunctionalInterface
-public interface DogQuerier {
-	public boolean test(Dog d);
-}
--- lambda-examples/src/main/java/com/example7/TriPredicate.java
+++
@@ -1,6 +0,0 @@
-package com.example7;
-
-@FunctionalInterface
-interface TriPredicate<T, U, V> {
-	boolean test(T t, U u, V v);
-}
